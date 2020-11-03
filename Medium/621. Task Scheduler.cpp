2e433041c9d6deb5b29439f9a6eca0816df26342
omarhosny206 <omarhosny206@domain.com>
2020-11-03 13:12:08 +0200

    LeetCode Problems

--- Medium/621. Task Scheduler.cpp
+++ Medium/621. Task Scheduler.cpp
@@ -18,22 +18,22 @@ public:
 
         while (!task_maxHeap.empty())
         {
-            vector<int> runing_task;
+            vector<int> running_task;
 
             for (int i = 0; i < n + 1; ++i)
             {
                 if (!task_maxHeap.empty())
                 {
-                    runing_task.push_back(task_maxHeap.top() - 1);
+                    running_task.push_back(task_maxHeap.top() - 1);
                     task_maxHeap.pop();
                 }
             }
 
-            for (int task : runing_task)
+            for (int task : running_task)
                 if (task)
                     task_maxHeap.push(task);
 
-            cycles += (task_maxHeap.empty() ? runing_task.size() : n + 1);
+            cycles += (task_maxHeap.empty() ? running_task.size() : n + 1);
         }
 
         return cycles;
