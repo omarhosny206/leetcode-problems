4a609658217da450f2167c9994ec9838365a35f4
omarhosny206 <omarhosny206@domain.com>
2020-11-19 15:56:18 +0200

    LeetCode Problems

--- Easy/860. Lemonade Change.cpp
+++ Easy/860. Lemonade Change.cpp
@@ -1,32 +1,38 @@
 // https://leetcode.com/problems/lemonade-change/
 
-class Solution {
+class Solution
+{
 public:
-    bool lemonadeChange(vector<int>& bills) {
+    bool lemonadeChange(vector<int> &bills)
+    {
         int five = 0, ten = 0;
-        
-        for(int bill : bills) {     
-           if(bill == 5)
-               five++;
-           else if(bill == 10) {
-               if(five == 0)
-                   return false;
-               five--;
-               ten++;
-           }
-            
-           else {
-               if(five > 0 && ten > 0) {
-                   five--;
-                   ten--;
-               }
-               else if(five >= 3)
-                   five -= 3;
-               else
-                   return false;
-           }
+
+        for (int bill : bills)
+        {
+            if (bill == 5)
+                five++;
+            else if (bill == 10)
+            {
+                if (five == 0)
+                    return false;
+                five--;
+                ten++;
+            }
+
+            else
+            {
+                if (five > 0 && ten > 0)
+                {
+                    five--;
+                    ten--;
+                }
+                else if (five >= 3)
+                    five -= 3;
+                else
+                    return false;
+            }
         }
-        
+
         return true;
     }
 };
