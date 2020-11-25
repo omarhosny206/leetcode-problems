5551ae189a08f294cb1157956290e81a870ed3f1
omarhosny206 <omarhosny206@domain.com>
2020-11-25 20:19:34 +0200

    LeetCode Problems

--- Easy/665. Non-decreasing Array.cpp
+++ Easy/665. Non-decreasing Array.cpp
@@ -5,23 +5,24 @@ class Solution
 public:
     bool checkPossibility(vector<int> &nums)
     {
-        bool oneChangeMade = false;
+        int counter = 0;
         for (int i = 1; i < nums.size(); ++i)
         {
-            if (nums[i - 1] > nums[i])
+            if (nums[i] < nums[i - 1])
             {
-                if (oneChangeMade)
-                    return false;
-
-                oneChangeMade = true;
-
-                if (i == nums.size() - 1 || i == 1)
-                    continue;
-
-                else if (!(nums[i] >= nums[i - 2] || nums[i + 1] >= nums[i - 1]))
-                    return false;
+                if (i == 1 || nums[i] >= nums[i - 2])
+                {
+                    nums[i - 1] = nums[i];
+                    counter++;
+                }
+                else
+                {
+                    nums[i] = nums[i - 1];
+                    counter++;
+                }
             }
         }
-        return true;
+
+        return counter <= 1;
     }
 };
