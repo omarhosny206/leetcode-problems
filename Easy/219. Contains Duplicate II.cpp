d5177fcb1f46ef8f00610ea32097d3f8a4b8467a
omarhosny206 <omarhosny206@domain.com>
2021-05-11 15:13:57 +0200

    LeetCode Problems

--- Easy/219. Contains Duplicate II.cpp
+++ Easy/219. Contains Duplicate II.cpp
@@ -6,12 +6,13 @@ public:
     bool containsNearbyDuplicate(vector<int> &nums, int k)
     {
         unordered_map<int, int> numbers;
+
         for (int i = 0; i < nums.size(); ++i)
         {
             if (numbers.find(nums[i]) != numbers.end() && i - numbers[nums[i]] <= k)
                 return true;
-            else
-                numbers[nums[i]] = i;
+
+            numbers[nums[i]] = i;
         }
 
         return false;
