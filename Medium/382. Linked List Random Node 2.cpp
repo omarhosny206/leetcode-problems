0cf76e9326b9b359bad6254bc63411acde51a412
omarhosny206 <omarhosny206@domain.com>
2022-01-07 13:53:08 +0200

    LeetCode Problems

--- Medium/382. Linked List Random Node 2.cpp
+++ Medium/382. Linked List Random Node 2.cpp
@@ -12,21 +12,33 @@
  */
 class Solution
 {
-    vector<int> nums;
+    ListNode *head = nullptr;
 
 public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
     Solution(ListNode *head)
     {
-        while (head != nullptr)
-        {
-            nums.push_back(head->val);
-            head = head->next;
-        }
+        this->head = head;
     }
 
+    /** Returns a random node's value. */
     int getRandom()
     {
-        return nums[rand() % nums.size()];
+        ListNode *result;
+        ListNode *current = head;
+        int i = 1;
+
+        while (current != nullptr)
+        {
+            if (rand() % i == 0)
+                result = current;
+
+            current = current->next;
+            i++;
+        }
+
+        return result->val;
     }
 };
 
