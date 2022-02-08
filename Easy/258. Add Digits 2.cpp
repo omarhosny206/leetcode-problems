50e613b8498d9686c2531a08c51aea5a1aa91029
omarhosny206 <omarhosny206@domain.com>
2022-02-08 14:40:17 +0200

    LeetCode Problems

--- Easy/258. Add Digits 2.cpp
+++ Easy/258. Add Digits 2.cpp
@@ -13,14 +13,9 @@ public:
 
     int getDigitsSum(int num)
     {
-        int result = 0;
+        if (num == 0)
+            return 0;
 
-        while (num > 0)
-        {
-            result += num % 10;
-            num /= 10;
-        }
-
-        return result;
+        return num % 10 + getDigitsSum(num / 10);
     }
 };
