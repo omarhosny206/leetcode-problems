d5c501e61a6f8fedd9469264d9cad3634f0d32a6
omarhosny206 <omarhosny206@domain.com>
2022-02-17 20:49:42 +0200

    LeetCode Problems

--- Medium/39. Combination Sum.cpp
+++ Medium/39. Combination Sum.cpp
@@ -2,28 +2,34 @@
 
 class Solution
 {
+    vector<vector<int>> result;
+    vector<int> combination;
+
 public:
     vector<vector<int>> combinationSum(vector<int> &candidates, int target)
     {
-        vector<vector<int>> result;
-        vector<int> currentSum;
-        hasSumWithTarget(candidates, result, currentSum, target, 0);
+        dfs(candidates, target, 0, 0);
         return result;
     }
 
-    void hasSumWithTarget(vector<int> &candidates, vector<vector<int>> &result, vector<int> &currentSum, int target, int position)
+    void dfs(vector<int> &candidates, int target, int currentSum, int position)
     {
-        if (target < 0)
-            return;
-
-        if (target == 0)
-            result.push_back(currentSum);
-
-        for (int i = position; i < candidates.size(); i++)
+        if (currentSum > target)
         {
-            currentSum.push_back(candidates[i]);
-            hasSumWithTarget(candidates, result, currentSum, target - candidates[i], i);
-            currentSum.pop_back();
+            return;
+        }
+
+        if (currentSum == target)
+        {
+            result.push_back(combination);
+            return;
+        }
+
+        for (int i = position; i < candidates.size(); ++i)
+        {
+            combination.push_back(candidates[i]);
+            dfs(candidates, target, currentSum + candidates[i], i);
+            combination.pop_back();
         }
     }
 };
---
+++ Medium/752. Open the Lock.cpp
@@ -0,0 +1,73 @@
+// https://leetcode.com/problems/open-the-lock/
+
+class Solution
+{
+    queue<string> nodes;
+    unordered_set<string> blockedLocks;
+    unordered_set<string> visited;
+
+public:
+    int openLock(vector<string> &deadends, string target)
+    {
+        int numTurns = 0;
+        string start = "0000";
+
+        if (start == target)
+            return 0;
+
+        for (string &deadend : deadends)
+            blockedLocks.insert(deadend);
+
+        if (blockedLocks.find(start) != blockedLocks.end())
+            return -1;
+
+        nodes.push(start);
+        visited.insert(start);
+
+        while (!nodes.empty())
+        {
+            numTurns++;
+            int size = nodes.size();
+
+            for (int i = 0; i < size; ++i)
+            {
+                string current = nodes.front();
+                nodes.pop();
+
+                for (int j = 0; j < current.length(); ++j)
+                {
+                    string temp = current;
+
+                    temp[j] = (((current[j] - '0') + 1) % 10) + '0';
+
+                    if (temp == target)
+                        return numTurns;
+
+                    if (isOk(temp))
+                    {
+                        nodes.push(temp);
+                        visited.insert(temp);
+                    }
+
+                    temp[j] = (((current[j] - '0') - 1 + 10) % 10) + '0';
+
+                    if (temp == target)
+                        return numTurns;
+
+                    if (isOk(temp))
+                    {
+                        nodes.push(temp);
+                        visited.insert(temp);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    bool isOk(string &lock)
+    {
+        return (blockedLocks.find(lock) == blockedLocks.end()) && (visited.find(lock) == visited.end());
+    }
+};
