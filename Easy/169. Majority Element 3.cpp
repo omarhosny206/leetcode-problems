68b26f445ece266acf43bfad0d45ae9694fe1610
omarhosny206 <omarhosny206@domain.com>
2022-02-21 16:33:37 +0200

    LeetCode Problems

--- Easy/169. Majority Element 3.cpp
+++ Easy/169. Majority Element 3.cpp
@@ -5,25 +5,7 @@ class Solution
 public:
     int majorityElement(vector<int> &nums)
     {
-        if (nums.size() <= 2)
-            return nums[0];
-
-        int counter = 1;
-
         sort(nums.begin(), nums.end());
-
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            if (nums[i] == nums[i - 1])
-                counter++;
-
-            else
-                counter = 1;
-
-            if (counter > nums.size() / 2)
-                return nums[i];
-        }
-
-        return -1;
+        return nums[nums.size() / 2];
     }
 };
---
+++ Easy/169. Majority Element 4.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/majority-element/
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        if (nums.size() <= 2)
+            return nums[0];
+
+        int counter = 1;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] == nums[i - 1])
+                counter++;
+
+            else
+                counter = 1;
+
+            if (counter > nums.size() / 2)
+                return nums[i];
+        }
+
+        return -1;
+    }
+};
