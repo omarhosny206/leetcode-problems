0bec149142ea17b14135d7961b4ee89e69bf089c
omarhosny206 <omarhosny102@gmail.com>
2022-03-13 13:32:39 +0200

    LeetCode Problems

--- Easy/20. Valid Parentheses 2.cpp
+++ Easy/20. Valid Parentheses 2.cpp
@@ -5,6 +5,7 @@ class Solution
 public:
     bool isValid(string s)
     {
+        stack<char> parentheses;
 
         map<char, int> mapping{
             {'(', 1},
@@ -14,20 +15,20 @@ public:
             {'[', 5},
             {']', 6}};
 
-        stack<char> parentheses_stack;
-        for (char c : s)
+        for (char &c : s)
         {
             if (mapping[c] % 2 != 0)
-                parentheses_stack.push(c);
+                parentheses.push(c);
 
             else
             {
-                if (parentheses_stack.empty() || mapping[c] != mapping[parentheses_stack.top()] + 1)
+                if (parentheses.empty() || mapping[c] != mapping[parentheses.top()] + 1)
                     return false;
-                parentheses_stack.pop();
+
+                parentheses.pop();
             }
         }
 
-        return parentheses_stack.size() == 0;
+        return parentheses.empty();
     }
 };
