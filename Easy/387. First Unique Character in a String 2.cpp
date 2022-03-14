99b6fa1a9ee40934fefa845982e4e8e83d598a90
omarhosny206 <omarhosny102@gmail.com>
2022-03-14 17:57:38 +0200

    LeetCode Problems

--- Easy/387. First Unique Character in a String 2.cpp
+++ Easy/387. First Unique Character in a String 2.cpp
@@ -5,13 +5,13 @@ class Solution
 public:
     int firstUniqChar(string s)
     {
-        vector<int> freq(26);
+        unordered_map<char, int> freq;
 
         for (int i = 0; i < s.length(); ++i)
-            freq[s[i] - 'a']++;
+            freq[s[i]]++;
 
         for (int i = 0; i < s.length(); ++i)
-            if (freq[s[i] - 'a'] == 1)
+            if (freq[s[i]] == 1)
                 return i;
 
         return -1;
