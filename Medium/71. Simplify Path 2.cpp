11a4af4c2d66bda597e7284fa8aedde42ae9340a
omarhosny206 <omarhosny102@gmail.com>
2022-03-14 17:47:39 +0200

    LeetCode Problems

---
+++ Medium/71. Simplify Path 2.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/simplify-path/
+
+class Solution
+{
+public:
+    string simplifyPath(string path)
+    {
+        string result = "";
+
+        stack<string> directories = getDirectories(path);
+
+        if (directories.empty())
+            return "/";
+
+        while (!directories.empty())
+        {
+            result = "/" + directories.top() + result;
+            directories.pop();
+        }
+
+        return result;
+    }
+
+    stack<string> getDirectories(string &path)
+    {
+        stack<string> directories;
+        string currentDirectory = "";
+
+        path = path + "/";
+
+        for (char &c : path)
+        {
+            if (c == '/')
+            {
+                if (currentDirectory == "..")
+                {
+                    if (!directories.empty())
+                        directories.pop();
+                }
+
+                else if (currentDirectory != "" && currentDirectory != ".")
+                    directories.push(currentDirectory);
+
+                currentDirectory = "";
+            }
+
+            else
+                currentDirectory += c;
+        }
+
+        return directories;
+    }
+};
