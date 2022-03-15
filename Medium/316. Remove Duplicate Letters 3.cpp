dd478836735222c478e565e967a24ef098aee767
omarhosny206 <omarhosny102@gmail.com>
2022-03-15 14:11:08 +0200

    LeetCode Problems

---
+++ Medium/316. Remove Duplicate Letters 3.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/remove-duplicate-letters/
+
+class Solution
+{
+public:
+    string removeDuplicateLetters(string s)
+    {
+        string result = "";
+
+        vector<int> freq(26);
+        vector<bool> visited(26);
+        stack<char> letters;
+
+        for (char &c : s)
+            freq[c - 'a']++;
+
+        for (char &c : s)
+        {
+            freq[c - 'a']--;
+
+            if (visited[c - 'a'])
+                continue;
+
+            while (!letters.empty() && c < letters.top() && freq[letters.top() - 'a'] > 0)
+            {
+                visited[letters.top() - 'a'] = false;
+                letters.pop();
+            }
+
+            letters.push(c);
+            visited[c - 'a'] = true;
+        }
+
+        while (!letters.empty())
+        {
+            result += letters.top();
+            letters.pop();
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
