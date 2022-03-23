5ffcdc4cb8e06ed91c0c6257926835b207016f75
omarhosny206 <omarhosny102@gmail.com>
2022-03-23 08:40:14 +0200

    LeetCode Problems

---
+++ Medium/991. Broken Calculator.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/broken-calculator/
+
+class Solution
+{
+public:
+    int brokenCalc(int startValue, int target)
+    {
+        int result = 0;
+
+        while (startValue < target)
+        {
+            if (target % 2 == 0)
+                target /= 2;
+
+            else
+                target++;
+
+            result++;
+        }
+
+        return result + startValue - target;
+    }
+};
