a7ef55b59206deb46b38ec60e609507390ed291e
omarhosny206 <omarhosny102@gmail.com>
2022-03-24 11:26:23 +0200

    LeetCode Problems

--- Medium/881. Boats to Save People.cpp
+++ Medium/881. Boats to Save People.cpp
@@ -8,13 +8,14 @@ public:
         int result = 0;
         int i = 0;
         int j = people.size() - 1;
-        
+
         sort(people.begin(), people.end());
 
         while (i <= j)
         {
             if (people[i] + people[j] <= limit)
                 i++;
+
             j--;
             result++;
         }
