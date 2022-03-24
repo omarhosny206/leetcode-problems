e9d406dd0a30307478a274bb37f9889d9be25b29
omarhosny206 <omarhosny102@gmail.com>
2022-03-24 12:23:37 +0200

    LeetCode Problems

---
+++ Medium/881. Boats to Save People 2.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/boats-to-save-people/
+
+class Solution
+{
+public:
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+        int result = 0;
+        int i = 0;
+        int j = people.size() - 1;
+
+        sort(people.begin(), people.end());
+
+        while (i <= j)
+        {
+            if (people[i] + people[j] <= limit)
+            {
+                i++;
+                j--;
+            }
+
+            else
+                j--;
+
+            result++;
+        }
+
+        return result;
+    }
+};
---
+++ Medium/881. Boats to Save People 3.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/boats-to-save-people/
+
+class Solution
+{
+public:
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+        int result = 0;
+        int i = 0;
+        int j = people.size() - 1;
+
+        sort(people.begin(), people.end());
+
+        while (i <= j)
+        {
+            if (people[i] + people[j] <= limit)
+                i++;
+
+            j--;
+            result++;
+        }
+
+        return result;
+    }
+};
