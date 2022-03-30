be34498f7eb5f9b9b55c85fe5009174021605900
omarhosny206 <omarhosny102@gmail.com>
2022-03-30 10:35:40 +0200

    LeetCode Problems

---
+++ Medium/74. Search a 2D Matrix 2.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int row = getRow(matrix, 0, matrix.size() - 1, target);
+        return binarySearch(matrix, row, 0, matrix[0].size() - 1, target);
+    }
+
+    bool binarySearch(vector<vector<int>> &matrix, int row, int left, int right, int target)
+    {
+        if (left > right)
+            return false;
+
+        int middle = left + (right - left) / 2;
+
+        if (matrix[row][middle] == target)
+            return true;
+
+        if (matrix[row][middle] > target)
+            return binarySearch(matrix, row, left, middle - 1, target);
+
+        return binarySearch(matrix, row, middle + 1, right, target);
+    }
+
+    int getRow(vector<vector<int>> &matrix, int top, int bottom, int target)
+    {
+        if (top > bottom)
+            return 0;
+
+        int middle = top + (bottom - top) / 2;
+
+        if (matrix[middle][0] <= target && matrix[middle][matrix[middle].size() - 1] >= target)
+            return middle;
+
+        if (matrix[middle][0] > target)
+            return getRow(matrix, top, middle - 1, target);
+
+        return getRow(matrix, middle + 1, bottom, target);
+    }
+};
