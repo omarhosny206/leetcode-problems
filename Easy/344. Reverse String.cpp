0f6ccdd6162092bed3beaf1935ee29ecfa602109
omarhosny206 <omarhosny102@gmail.com>
2022-04-01 10:36:20 +0200

    LeetCode Problems

--- Easy/344. Reverse String.cpp
+++ Easy/344. Reverse String.cpp
@@ -5,14 +5,19 @@ class Solution
 public:
     void reverseString(vector<char> &s)
     {
+        reverse(s);
+    }
+
+    void reverse(vector<char> &letters)
+    {
         int i = 0;
-        int j = s.size() - 1;
+        int j = letters.size() - 1;
 
         while (i < j)
         {
-            char temp = s[i];
-            s[i++] = s[j];
-            s[j--] = temp;
+            char temp = letters[i];
+            letters[i++] = letters[j];
+            letters[j--] = temp;
         }
     }
 };
