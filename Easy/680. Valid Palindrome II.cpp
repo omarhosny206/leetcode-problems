4d70f39e89c8208aa0a4eba0b603050b3a1df177
omarhosny206 <omarhosny102@gmail.com>
2022-04-02 13:03:51 +0200

    LeetCode Problems

---
+++ Easy/680. Valid Palindrome II.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/valid-palindrome-ii/
+
+class Solution
+{
+public:
+    bool validPalindrome(string s)
+    {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j)
+        {
+            if (s[i] != s[j])
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    bool isPalindrome(string s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i] != s[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
