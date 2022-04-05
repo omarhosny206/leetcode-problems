fdcbe7c50699eaf785b53935ee738ba02fefe6c2
omarhosny206 <omarhosny102@gmail.com>
2022-04-05 10:27:48 +0200

    LeetCode Problems

--- Medium/11. Container With Most Water.cpp
+++ Medium/11. Container With Most Water.cpp
@@ -5,21 +5,28 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
+        int result = 0;
         int i = 0;
         int j = height.size() - 1;
-        int maxArea = 0;
 
         while (i < j)
         {
-            maxArea = max(maxArea, min(height[i], height[j]) * (j - i));
+            int area = calculateRectangleArea(min(height[i], height[j]), j - i);
 
-            if (height[i] < height[j])
-                i++;
+            result = max(result, area);
+
+            if (height[i] > height[j])
+                j--;
 
             else
-                j--;
+                i++;
         }
 
-        return maxArea;
+        return result;
+    }
+
+    int calculateRectangleArea(int length, int width)
+    {
+        return length * width;
     }
 };
