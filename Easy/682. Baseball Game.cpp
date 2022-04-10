0ef1d99fecdebc60ebf1e7e6adf8b80fe545ae6f
omarhosny206 <omarhosny102@gmail.com>
2022-04-10 15:54:05 +0200

    LeetCode Problems

---
+++ Easy/682. Baseball Game.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/baseball-game/
+
+class Solution
+{
+public:
+    int calPoints(vector<string> &ops)
+    {
+        int result = 0;
+        stack<int> nums;
+
+        for (string &operation : ops)
+        {
+            if (operation == "C")
+                nums.pop();
+
+            else if (operation == "D")
+                nums.push(nums.top() * 2);
+
+            else if (operation == "+")
+            {
+                int first = nums.top();
+                nums.pop();
+                int second = nums.top();
+                nums.push(first);
+                nums.push(first + second);
+            }
+
+            else
+                nums.push(stoi(operation));
+        }
+
+        while (!nums.empty())
+        {
+            result += nums.top();
+            nums.pop();
+        }
+
+        return result;
+    }
+};
