8ada4fa1b9a3cf5527b4e4a67800a787f69fa0fd
omarhosny206 <omarhosny102@gmail.com>
2022-04-11 12:02:32 +0200

    LeetCode Problems

---
+++ Easy/1260. Shift 2D Grid 2.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/shift-2d-grid/
+
+class Solution
+{
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>> &grid, int k)
+    {
+        vector<vector<int>> result(grid.size(), vector<int>(grid[0].size()));
+        vector<int> nums;
+
+        int position = 0;
+
+        for (vector<int> &row : grid)
+            for (int &num : row)
+                nums.push_back(num);
+
+        while (k--)
+        {
+            int num = nums.back();
+            nums.pop_back();
+            nums.insert(nums.begin(), num);
+        }
+
+        for (int i = 0; i < grid.size(); ++i)
+            for (int j = 0; j < grid[i].size(); ++j)
+                result[i][j] = nums[position++];
+
+        return result;
+    }
+};
