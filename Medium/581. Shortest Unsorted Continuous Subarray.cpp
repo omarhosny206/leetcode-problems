dc7bc4a758e433a4cab19dff6ba932289d1cdf24
omarhosny206 <omarhosny102@gmail.com>
2022-05-03 16:03:26 +0200

    LeetCode Problems

--- Medium/581. Shortest Unsorted Continuous Subarray.cpp
+++ Medium/581. Shortest Unsorted Continuous Subarray.cpp
@@ -5,9 +5,11 @@ class Solution
 public:
     int findUnsortedSubarray(vector<int> &nums)
     {
-        int start = nums.size();
-        int end = 0;
+        int start = INT_MAX;
+        int end = INT_MIN;
+
         vector<int> sortedNums = nums;
+
         sort(sortedNums.begin(), sortedNums.end());
 
         for (int i = 0; i < nums.size(); ++i)
@@ -19,7 +21,7 @@ public:
             }
         }
 
-        if (start == nums.size())
+        if (start == INT_MAX)
             return 0;
 
         return end - start + 1;
