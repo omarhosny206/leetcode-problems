033572260a834d2fb72dc2a80559e186b9d2f268
omarhosny206 <omarhosny102@gmail.com>
2022-05-10 19:52:39 +0200

    Added problem 216 2

---
+++ Medium/216. Combination Sum III 2.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/combination-sum-iii/
+
+class Solution
+{
+    vector<vector<int>> result;
+
+public:
+    vector<vector<int>> combinationSum3(int k, int n)
+    {
+        vector<int> current;
+        dfs(k, n, current, 0, 1);
+        return result;
+    }
+
+    void dfs(int k, int n, vector<int> &current, int currentSum, int index)
+    {
+        if (currentSum > n)
+            return;
+
+        if (current.size() == k && currentSum == n)
+        {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = index; i <= 9; ++i)
+        {
+            if (find(current.begin(), current.end(), i) == current.end())
+            {
+                current.push_back(i);
+                dfs(k, n, current, currentSum + i, i + 1);
+                current.pop_back();
+            }
+        }
+    }
+};
