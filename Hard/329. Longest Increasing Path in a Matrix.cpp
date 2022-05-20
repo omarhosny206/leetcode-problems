194de054a45a74100f47a962f0ad860e5a6a78f4
omarhosny206 <omarhosny102@gmail.com>
2022-05-20 22:55:26 +0200

    Updated problem 329

--- Hard/329. Longest Increasing Path in a Matrix.cpp
+++ Hard/329. Longest Increasing Path in a Matrix.cpp
@@ -5,32 +5,29 @@ class Solution
 public:
     int longestIncreasingPath(vector<vector<int>> &matrix)
     {
-        if (matrix.size() == 0)
-            return 0;
-
-        int answer = 0;
-        vector<vector<int>> DP(matrix.size(), vector<int>(matrix[0].size(), -1));
+        int result = 0;
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
 
         for (int i = 0; i < matrix.size(); ++i)
             for (int j = 0; j < matrix[i].size(); ++j)
-                answer = max(answer, DFS(matrix, DP, i, j, INT_MIN));
+                result = max(result, dfs(matrix, dp, i, j, INT_MIN));
 
-        return answer;
+        return result;
     }
 
-    int DFS(vector<vector<int>> &matrix, vector<vector<int>> &DP, int i, int j, int last_val)
+    int dfs(vector<vector<int>> &matrix, vector<vector<int>> &dp, int i, int j, int last_val)
     {
         if (i < 0 || i >= matrix.size() || j < 0 || j >= matrix[i].size() || matrix[i][j] <= last_val)
             return 0;
 
-        if (DP[i][j] != -1)
-            return DP[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        DP[i][j] = 1 + max({DFS(matrix, DP, i + 1, j, matrix[i][j]),
-                            DFS(matrix, DP, i - 1, j, matrix[i][j]),
-                            DFS(matrix, DP, i, j + 1, matrix[i][j]),
-                            DFS(matrix, DP, i, j - 1, matrix[i][j])});
+        dp[i][j] = 1 + max({dfs(matrix, dp, i + 1, j, matrix[i][j]),
+                            dfs(matrix, dp, i - 1, j, matrix[i][j]),
+                            dfs(matrix, dp, i, j + 1, matrix[i][j]),
+                            dfs(matrix, dp, i, j - 1, matrix[i][j])});
 
-        return DP[i][j];
+        return dp[i][j];
     }
 };
