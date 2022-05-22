c6e0f18103cbad0437c2853c4546d22198827f49
omarhosny206 <omarhosny102@gmail.com>
2022-05-22 16:09:28 +0200

    Added problem 647

---
+++ Medium/647. Palindromic Substrings.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/palindromic-substrings/
+
+class Solution
+{
+    int result = 0;
+
+public:
+    int countSubstrings(string s)
+    {
+        if (s.length() == 0)
+            return 0;
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            expandAroundCenter(s, i, i);
+            expandAroundCenter(s, i, i + 1);
+        }
+
+        return result;
+    }
+
+    int expandAroundCenter(string &s, int left, int right)
+    {
+        if (s.length() == 0 || left > right)
+            return 0;
+
+        while (left >= 0 && right < s.length() && s[left] == s[right])
+        {
+            result++;
+            left--;
+            right++;
+        }
+
+        return result;
+    }
+};
