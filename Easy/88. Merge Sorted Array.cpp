f082eabddae2ab77686483d4ff7222f26c781059
omarhosny206 <omarhosny102@gmail.com>
2022-06-07 11:09:22 +0200

    Updated problem 88

--- Easy/88. Merge Sorted Array.cpp
+++ Easy/88. Merge Sorted Array.cpp
@@ -5,20 +5,19 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
-
-        for (int index = nums1.size() - 1;
-             index >= 0; --index)
+        for (int i = nums1.size() - 1; i >= 0; --i)
         {
             if (m > 0 && n > 0)
             {
                 if (nums1[m - 1] >= nums2[n - 1])
-                    nums1[index] = nums1[--m];
+                    nums1[i] = nums1[--m];
 
                 else
-                    nums1[index] = nums2[--n];
+                    nums1[i] = nums2[--n];
             }
+
             else if (n > 0)
-                nums1[index] = nums2[--n];
+                nums1[i] = nums2[--n];
         }
     }
 };
