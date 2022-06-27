360f4ac6adf11217adaedd7e48b410613a007cba
omarhosny206 <omarhosny102@gmail.com>
2022-06-27 13:18:21 +0200

    Updated problems 1689

--- Medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ Medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -6,7 +6,8 @@ public:
     int minPartitions(string n)
     {
         int result;
-        for (char c : n)
+
+        for (char &c : n)
             result = max(result, c - '0');
 
         return result;
