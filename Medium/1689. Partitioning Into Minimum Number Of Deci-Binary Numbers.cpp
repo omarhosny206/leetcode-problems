3f33d4bb0f3bd0c92cb47791689086af76ca749c
omarhosny206 <omarhosny102@gmail.com>
2022-06-27 13:39:56 +0200

    Updated problems 1689

--- Medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ Medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -1,11 +1,10 @@
-// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
 
 class Solution
 {
 public:
     int minPartitions(string n)
     {
-        int result;
+        int result = INT_MIN;
 
         for (char &c : n)
             result = max(result, c - '0');
