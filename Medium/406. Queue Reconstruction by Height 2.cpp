c893ac098eba63a21d40d80318af48d6648e7e4b
omarhosny206 <omarhosny102@gmail.com>
2022-06-29 09:21:24 +0200

    Updated problem 406 and Added problem 406 2

---
+++ Medium/406. Queue Reconstruction by Height 2.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/queue-reconstruction-by-height/
+
+class Solution
+{
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
+    {
+        vector<vector<int>> result;
+        sort(people.begin(), people.end(), compare);
+
+        for (vector<int> &person : people)
+            result.insert(result.begin() + person[1], person);
+
+        return result;
+    }
+
+    static bool compare(vector<int> &a, vector<int> &b)
+    {
+        return ((a[0] > b[0]) || (a[0] == b[0] && a[1] < b[1]));
+    }
+};
--- Medium/406. Queue Reconstruction by Height.cpp
+++ Medium/406. Queue Reconstruction by Height.cpp
@@ -5,17 +5,37 @@ class Solution
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
     {
-        vector<vector<int>> result;
+        vector<vector<int>> result(people.size(), {-1, -1});
         sort(people.begin(), people.end(), compare);
 
-        for (vector<int> &person : people)
-            result.insert(result.begin() + person[1], person);
+        result[people[0][1]] = people[0];
+
+        for (int i = 1; i < people.size(); ++i)
+        {
+            vector<int> person = people[i];
+
+            int j = 0;
+            int counter = person[1];
+
+            while (counter > 0)
+            {
+                if (person[0] <= result[j][0] || result[j][0] == -1)
+                    counter--;
+
+                j++;
+            }
+
+            while (j < people.size() && result[j][0] != -1)
+                j++;
+
+            result[j] = person;
+        }
 
         return result;
     }
 
     static bool compare(vector<int> &a, vector<int> &b)
     {
-        return ((a[0] > b[0]) || (a[0] == b[0] && a[1] < b[1]));
+        return ((a[0] < b[0]) || (a[0] == b[0] && a[1] < b[1]));
     }
 };
