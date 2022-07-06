f4a83dd01ab806a57f445f45a7855f8350ff70f9
omarhosny206 <omarhosny102@gmail.com>
2022-07-06 17:07:09 +0200

    Updated problem 509 3

--- Easy/509. Fibonacci Number 3.cpp
+++ Easy/509. Fibonacci Number 3.cpp
@@ -3,14 +3,14 @@
 class Solution
 {
 public:
-    int fib(int N)
+    int fib(int n)
     {
-        if (N == 0)
+        if (n == 0)
             return 0;
 
-        if (N == 1)
+        if (n == 1)
             return 1;
 
-        return fib(N - 1) + fib(N - 2);
+        return fib(n - 1) + fib(n - 2);
     }
 };
