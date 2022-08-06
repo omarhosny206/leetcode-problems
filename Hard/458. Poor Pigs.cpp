bc7aff0bc94a33bd90da8d44d365db3f30c92b00
omarhosny206 <omarhosny102@gmail.com>
2022-08-06 22:11:14 +0200

    Added problem 458

---
+++ Hard/458. Poor Pigs.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/poor-pigs/
+
+class Solution
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
