2951b1490578ea6c7cb45fb605468255826a238f
omarhosny206 <omarhosny102@gmail.com>
2022-08-29 13:31:38 +0200

    Updated problem 200

--- Medium/200. Number of Islands.cpp
+++ Medium/200. Number of Islands.cpp
@@ -29,9 +29,9 @@ public:
 
         grid[i][j] = '0';
 
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 };
