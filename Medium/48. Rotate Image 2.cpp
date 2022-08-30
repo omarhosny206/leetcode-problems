a7b6c7781ea446f7f353a007734205aafe5bcad4
omarhosny206 <omarhosny102@gmail.com>
2022-08-30 13:52:34 +0200

    Updated problem 48 2

--- Medium/48. Rotate Image 2.cpp
+++ Medium/48. Rotate Image 2.cpp
@@ -5,18 +5,32 @@ class Solution
 public:
     void rotate(vector<vector<int>> &matrix)
     {
-        int size = matrix.size();
+        for (int i = 0; i < matrix.size(); ++i)
+            reverse(matrix[i]);
 
-        for (int i = 0; i < (size + 1) / 2; ++i)
+        for (int i = 0; i < matrix.size(); ++i)
         {
-            for (int j = 0; j < size / 2; ++j)
+            for (int j = 0; j < matrix[i].size() - i; ++j)
             {
-                int temp = matrix[size - 1 - j][i];
-                matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j];
-                matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i];
-                matrix[j][size - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+                int row = matrix.size() - j - 1;
+                int column = matrix.size() - i - 1;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[row][column];
+                matrix[row][column] = temp;
             }
+        }
+    }
+
+    void reverse(vector<int> &row)
+    {
+        int i = 0;
+        int j = row.size() - 1;
+
+        while (i < j)
+        {
+            int temp = row[i];
+            row[i++] = row[j];
+            row[j--] = temp;
         }
     }
 };
