483853f75bde80e51167727ebde12b03ea65e970
omarhosny206 <omarhosny102@gmail.com>
2022-09-09 18:10:23 +0200

    Added problem 1996

---
+++ Medium/1996. The Number of Weak Characters in the Game.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+
+class Solution
+{
+public:
+    int numberOfWeakCharacters(vector<vector<int>> &properties)
+    {
+        int result = 0;
+
+        sort(properties.begin(), properties.end(), compare);
+
+        int maxDefense = INT_MIN;
+
+        for (vector<int> &property : properties)
+        {
+            if (maxDefense > property[1])
+                result++;
+            else
+                maxDefense = property[1];
+        }
+
+        return result;
+    }
+
+    static bool compare(vector<int> &first, vector<int> &second)
+    {
+        return (first[0] > second[0]) || (first[0] == second[0] && first[1] < second[1]);
+    }
+};
