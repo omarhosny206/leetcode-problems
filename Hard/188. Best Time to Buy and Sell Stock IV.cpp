a885a81355b2309e4002ca26729bac87573a3158
omarhosny206 <omarhosny102@gmail.com>
2022-09-10 13:03:07 +0200

    Added problem 188

---
+++ Hard/188. Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+
+class Solution
+{
+public:
+    int maxProfit(int k, vector<int> &prices)
+    {
+        if (k == 0)
+            return 0;
+
+        vector<vector<int>> dp(k + 1, vector<int>(2));
+
+        for (int i = 0; i <= k; ++i)
+            dp[i][0] = INT_MAX;
+
+        for (int &price : prices)
+        {
+            for (int i = 1; i <= k; ++i)
+            {
+                dp[i][0] = min(dp[i][0], price - dp[i - 1][1]);
+                dp[i][1] = max(dp[i][1], price - dp[i][0]);
+            }
+        }
+
+        return dp[k][1];
+    }
+};
