c484620b88c54053b80737ecda4b6e7d316b9baa
omarhosny206 <omarhosny102@gmail.com>
2022-09-22 23:19:27 +0200

    Added problem 2416

---
+++ Hard/2416. Sum of Prefix Scores of Strings.cpp
@@ -0,0 +1,83 @@
+// https://leetcode.com/problems/sum-of-prefix-scores-of-strings/
+
+class TrieNode
+{
+public:
+    char val;
+    int counter;
+    vector<TrieNode *> children;
+
+    TrieNode()
+    {
+        this->counter = 0;
+        children = vector<TrieNode *>(26);
+    }
+
+    TrieNode(char val)
+    {
+        this->val = val;
+        this->counter = 0;
+        children = vector<TrieNode *>(26);
+    }
+};
+
+class Trie
+{
+private:
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        this->root = new TrieNode();
+    }
+
+    void insert(string &word)
+    {
+        TrieNode *current = root;
+
+        for (char &c : word)
+        {
+            if (current->children[c - 'a'] == nullptr)
+                current->children[c - 'a'] = new TrieNode(c);
+
+            current = current->children[c - 'a'];
+            current->counter++;
+        }
+    }
+
+    int getScore(string &word)
+    {
+        int result = 0;
+        TrieNode *current = root;
+
+        for (char &c : word)
+        {
+            current = current->children[c - 'a'];
+            result += current->counter;
+        }
+
+        return result;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> sumPrefixScores(vector<string> &words)
+    {
+        vector<int> result;
+        Trie *trie = new Trie();
+
+        for (string &word : words)
+            trie->insert(word);
+
+        for (string &word : words)
+        {
+            int score = trie->getScore(word);
+            result.push_back(score);
+        }
+
+        return result;
+    }
+};
