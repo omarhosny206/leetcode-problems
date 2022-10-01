2c945a157d45b707817e3fb3b924ad87df49a45e
omarhosny206 <omarhosny102@gmail.com>
2022-10-01 12:58:53 +0200

    Updated problem 91

--- Medium/91. Decode Ways.cpp
+++ Medium/91. Decode Ways.cpp
@@ -5,17 +5,16 @@ class Solution
 public:
     int numDecodings(string s)
     {
-
         if (s.length() == 0 || s[0] == '0')
             return 0;
 
         vector dp(s.length() + 1, 0);
+
         dp[0] = 1;
         dp[1] = s[0] == '0' ? 0 : 1;
 
         for (int i = 2; i <= s.length(); ++i)
         {
-
             int oneDigit = stoi(s.substr(i - 1, 1));
             int twoDigits = stoi(s.substr(i - 2, 2));
 
