a5ff258276c3027658a3bffcd04627ce3b5901b8
omarhosny206 <omarhosny102@gmail.com>
2022-10-02 17:35:48 +0200

    Added problem 2423 2

---
+++ Easy/2423. Remove Letter To Equalize Frequency 2.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+
+class Solution
+{
+public:
+    bool equalFrequency(string word)
+    {
+        unordered_map<char, int> freq;
+
+        for (char &c : word)
+            freq[c]++;
+
+        int maxValue = INT_MIN;
+        int minValue = INT_MAX;
+
+        for (auto &pair : freq)
+        {
+            if (pair.second > maxValue)
+                maxValue = pair.second;
+
+            if (pair.second < minValue)
+                minValue = pair.second;
+        }
+
+        if (maxValue > minValue + 1)
+            return false;
+
+        for (auto &pair : freq)
+            if (pair.second != maxValue && pair.second != minValue)
+                return false;
+
+        if (maxValue == minValue && minValue == 1)
+            return true;
+
+        int maxValueCounter = 0;
+        int minValueCounter = 0;
+
+        for (auto &pair : freq)
+        {
+            if (pair.second == maxValue)
+                maxValueCounter++;
+
+            if (pair.second == minValue)
+                minValueCounter++;
+        }
+
+        if (minValueCounter > maxValueCounter)
+            return maxValueCounter == 1;
+
+        return minValueCounter == 1;
+    }
+};
