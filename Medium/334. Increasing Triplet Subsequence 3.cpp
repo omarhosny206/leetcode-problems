3eadd26915b334bcb0b10fe75539f3b3a2ad8d3f
omarhosny206 <omarhosny102@gmail.com>
2022-10-11 18:58:51 +0200

    Added problem 334 3

---
+++ Medium/334. Increasing Triplet Subsequence 3.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+        vector<int> rightMax(nums.size());
+        vector<int> leftMin(nums.size());
+        int maxValue = INT_MIN;
+        int minValue = INT_MAX;
+
+        int right = nums.size() - 1;
+        int left = 0;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            maxValue = max(maxValue, nums[right - i]);
+            rightMax[right - i] = maxValue;
+
+            minValue = min(minValue, nums[left + i]);
+            leftMin[left + i] = minValue;
+        }
+
+        for (int i = 1; i < nums.size() - 1; ++i)
+            if (nums[i] > leftMin[i] && nums[i] < rightMax[i])
+                return true;
+
+        return false;
+    }
+};
