8d0b1df75e08e4791e59dc0a42899160fe7772d1
omarhosny206 <omarhosny102@gmail.com>
2022-10-19 19:56:49 +0200

    Updated problem 692

--- Medium/692. Top K Frequent Words.cpp
+++ Medium/692. Top K Frequent Words.cpp
@@ -2,31 +2,33 @@
 
 class Solution
 {
+    struct Comparator
+    {
+        bool operator()(pair<int, string> &a, pair<int, string> &b)
+        {
+            return (a.first < b.first) || ((a.first == b.first) && (a.second > b.second));
+        }
+    };
+
 public:
     vector<string> topKFrequent(vector<string> &words, int k)
     {
         vector<string> result;
-        map<string, int> freq;
-        for (string word : words)
+        unordered_map<string, int> freq;
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Comparator> vals;
+
+        for (string &word : words)
             freq[word]++;
 
-        auto comp = [](pair<int, string> &a, pair<int, string> &b) {
-            if (a.first != b.first)
-            {
-                return a.first < b.first;
-            }
-            return a.second > b.second;
-        };
-
-        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(comp)> maxHeap(comp);
-
-        for (auto pair : freq)
-            maxHeap.push({pair.second, pair.first});
+        for (auto &pair : freq)
+            vals.push({pair.second, pair.first});
 
         while (k--)
         {
-            result.push_back(maxHeap.top().second);
-            maxHeap.pop();
+            string value = vals.top().second;
+            vals.pop();
+
+            result.push_back(value);
         }
 
         return result;
