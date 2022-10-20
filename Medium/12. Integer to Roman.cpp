ba80029bdf7605e4c35ba082a021f5f5a79d6780
omarhosny206 <omarhosny102@gmail.com>
2022-10-20 16:56:04 +0200

    Updated problem 12

--- Medium/12. Integer to Roman.cpp
+++ Medium/12. Integer to Roman.cpp
@@ -6,16 +6,19 @@ public:
     string intToRoman(int num)
     {
         string result = "";
-        int vals[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string roman[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        vector<int> vals = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        for (int i = 0; num > 0; ++i)
+        int i = 0;
+        while (num > 0)
         {
             while (num >= vals[i])
             {
-                result += roman[i];
                 num -= vals[i];
+                result += romanSymbols[i];
             }
+
+            i++;
         }
 
         return result;
