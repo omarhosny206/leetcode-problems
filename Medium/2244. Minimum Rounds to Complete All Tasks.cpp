8ad2b244c18f6129acef7c27c5325ae9570d56ce
omarhosny206 <omarhosny102@gmail.com>
2022-10-24 15:05:02 +0200

    Added problem 2244

---
+++ Medium/2244. Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
+
+class Solution
+{
+public:
+    int minimumRounds(vector<int> &tasks)
+    {
+        int result = 0;
+        unordered_map<int, int> freq;
+
+        for (int &task : tasks)
+            freq[task]++;
+
+        for (auto &pair : freq)
+        {
+            if (pair.second == 1)
+                return -1;
+
+            else if (pair.second % 3 == 0)
+                result += pair.second / 3;
+
+            else
+                result += (pair.second / 3) + 1;
+        }
+
+        return result;
+    }
+};
