b9f832495a8a0d12f7aba5547b82c85a93640d19
omarhosny206 <omarhosny102@gmail.com>
2022-10-24 21:43:06 +0200

    Updated problem 1239

--- Medium/1239. Maximum Length of a Concatenated String with Unique Characters.cpp
+++ Medium/1239. Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -7,47 +7,40 @@ class Solution
 public:
     int maxLength(vector<string> &arr)
     {
-        dfs(arr, {}, 0, "");
+        dfs(arr, "", 0);
         return result;
     }
 
-    void dfs(vector<string> &arr, vector<int> freq, int i, string current)
+    void dfs(vector<string> &arr, string current, int index)
     {
-        if (!isOk(freq))
+        if (!hasUniqueCharacters(current))
             return;
 
-        result = max(result, (int)current.length());
+        result = max(result, (int)current.size());
 
-        if (i == arr.size())
+        if (index == arr.size())
             return;
 
-        for (int j = i; j < arr.size(); ++j)
+        for (int i = index; i < arr.size(); ++i)
         {
             string previous = current;
-            current += arr[j];
-
-            freq = countFreq(current);
-
-            dfs(arr, freq, j + 1, current);
+            current += arr[i];
+            dfs(arr, current, i + 1);
             current = previous;
         }
     }
 
-    vector<int> countFreq(string current)
+    bool hasUniqueCharacters(string current)
     {
         vector<int> freq(26);
 
         for (char &c : current)
+        {
             freq[c - 'a']++;
 
-        return freq;
-    }
-
-    bool isOk(vector<int> freq)
-    {
-        for (int i = 0; i < freq.size(); ++i)
-            if (freq[i] > 1)
+            if (freq[c - 'a'] > 1)
                 return false;
+        }
 
         return true;
     }
