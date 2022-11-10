8e24c7f28ea612398ac60fca9239508a5d478b5e
omarhosny206 <omarhosny102@gmail.com>
2022-11-10 07:27:59 +0200

    Updated problem 345 2

--- Easy/345. Reverse Vowels of a String 2.cpp
+++ Easy/345. Reverse Vowels of a String 2.cpp
@@ -5,31 +5,33 @@ class Solution
 public:
     string reverseVowels(string s)
     {
-        unordered_set<char> vowels;
-        vowels.insert('a');
-        vowels.insert('e');
-        vowels.insert('i');
-        vowels.insert('o');
-        vowels.insert('u');
-
         string result = "";
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
         int i = 0;
         int j = s.length() - 1;
+
         while (i < j)
         {
             while (i < j && vowels.find(tolower(s[i])) == vowels.end())
                 i++;
+
             while (i < j && vowels.find(tolower(s[j])) == vowels.end())
                 j--;
 
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+            swap(s[i], s[j]);
 
             i++;
             j--;
         }
 
         return s;
+    }
+
+    void swap(char &first, char &second)
+    {
+        char temp = first;
+        first = second;
+        second = temp;
     }
 };
