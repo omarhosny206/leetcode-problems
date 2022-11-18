d281690cae61875e4b998a01fa69d7da91b2daf6
omarhosny206 <omarhosny102@gmail.com>
2022-11-18 13:37:01 +0200

    Added problem 263

---
+++ Easy/263. Ugly Number.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/ugly-number/
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n == 0)
+            return false;
+
+        while (n % 5 == 0)
+            n = n / 5;
+
+        while (n % 3 == 0)
+            n = n / 3;
+
+        while (n % 2 == 0)
+            n = n / 2;
+
+        return n == 1;
+    }
+};
