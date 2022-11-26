65d4a2c4fa16d805bc18dc29a63a975a8f146c26
omarhosny206 <omarhosny102@gmail.com>
2022-11-26 18:20:56 +0200

    Added problem 2481

---
+++ Easy/2481. Minimum Cuts to Divide a Circle.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+
+class Solution
+{
+public:
+    int numberOfCuts(int n)
+    {
+        if (n == 1)
+            return 0;
+
+        return n % 2 == 0 ? n / 2 : n;
+    }
+};
