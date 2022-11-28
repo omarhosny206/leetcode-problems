bf550c4663839bbac8eecff8b34fb6588aef90d7
omarhosny206 <omarhosny102@gmail.com>
2022-11-28 13:51:32 +0200

    Added problem 2225

---
+++ Medium/2225. Find Players With Zero or One Losses.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-players-with-zero-or-one-losses/
+
+class Solution
+{
+public:
+    vector<vector<int>> findWinners(vector<vector<int>> &matches)
+    {
+        vector<vector<int>> result(2);
+        unordered_map<int, int> lostMatches;
+        unordered_set<int> winners;
+
+        for (vector<int> &match : matches)
+        {
+            winners.insert(match[0]);
+            lostMatches[match[1]]++;
+        }
+
+        for (const int &winner : winners)
+            if (lostMatches[winner] == 0)
+                result[0].push_back(winner);
+
+        for (auto &pair : lostMatches)
+            if (pair.second == 1)
+                result[1].push_back(pair.first);
+
+        sort(result[0].begin(), result[0].end());
+        sort(result[1].begin(), result[1].end());
+
+        return result;
+    }
+};
