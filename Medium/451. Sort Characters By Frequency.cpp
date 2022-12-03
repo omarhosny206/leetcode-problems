54212014b344507ea30ace217a619ff4cf152902
omarhosny206 <omarhosny102@gmail.com>
2022-12-03 14:55:27 +0200

    Updated problem 451

--- Medium/451. Sort Characters By Frequency.cpp
+++ Medium/451. Sort Characters By Frequency.cpp
@@ -7,22 +7,20 @@ public:
     {
         string result = "";
         unordered_map<char, int> freq;
-        priority_queue<pair<int, char>> max_heap;
+        priority_queue<pair<int, char>> maxHeap;
 
-        for (char c : s)
+        for (char &c : s)
             freq[c]++;
 
-        for (auto pair : freq)
-            max_heap.push({pair.second, pair.first});
+        for (auto &pair : freq)
+            maxHeap.push({pair.second, pair.first});
 
-        while (!max_heap.empty())
+        while (!maxHeap.empty())
         {
-            int k = max_heap.top().first;
+            pair<int, char> value = maxHeap.top();
+            maxHeap.pop();
 
-            while (k--)
-                result += max_heap.top().second;
-
-            max_heap.pop();
+            result += string(value.first, value.second);
         }
 
         return result;
