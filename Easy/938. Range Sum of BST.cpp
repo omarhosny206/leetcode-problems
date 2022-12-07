ad7eeb0a0cf1e864722e6b6fffd14ff796f81080
omarhosny206 <omarhosny102@gmail.com>
2022-12-07 14:25:46 +0200

    Added problem 938

--- Easy/938. Range Sum of BST.cpp
+++ Easy/938. Range Sum of BST.cpp
@@ -30,10 +30,10 @@ public:
         if (root->val >= low && root->val <= high)
             result += root->val;
 
-        if (root->val >= low)
+        if (root->val > low)
             dfs(root->left, low, high);
 
-        if (root->val <= high)
+        if (root->val < high)
             dfs(root->right, low, high);
     }
 };
