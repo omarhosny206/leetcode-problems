1b6f472342cc54d6aa62b22d0ff06185a918c21a
omarhosny206 <omarhosny102@gmail.com>
2022-12-13 18:28:50 +0200

    Updated problem 931

--- Medium/931. Minimum Falling Path Sum.cpp
+++ Medium/931. Minimum Falling Path Sum.cpp
@@ -7,29 +7,28 @@ public:
     {
         int result = INT_MAX;
         vector<vector<int>> memo(matrix.size(), vector<int>(matrix[0].size(), INT_MAX));
-        int i = 0;
 
         for (int j = 0; j < matrix[0].size(); ++j)
-            result = min(result, getPathValue(matrix, memo, i, j));
+            result = min(result, dfs(matrix, memo, 0, j));
 
         return result;
     }
 
-    int getPathValue(vector<vector<int>> &matrix, vector<vector<int>> &memo, int i, int j)
+    int dfs(vector<vector<int>> &matrix, vector<vector<int>> &memo, int i, int j)
     {
-        if (i >= matrix.size())
+        if (i == matrix.size())
             return 0;
 
-        if (j < 0 || j >= matrix[0].size())
-            return 1e+8;
+        if (i >= matrix.size() || i < 0 || j >= matrix[i].size() || j < 0)
+            return 1e8;
 
         if (memo[i][j] != INT_MAX)
             return memo[i][j];
 
         memo[i][j] = min({memo[i][j],
-                          matrix[i][j] + getPathValue(matrix, memo, i + 1, j),
-                          matrix[i][j] + getPathValue(matrix, memo, i + 1, j - 1),
-                          matrix[i][j] + getPathValue(matrix, memo, i + 1, j + 1)});
+                          matrix[i][j] + dfs(matrix, memo, i + 1, j),
+                          matrix[i][j] + dfs(matrix, memo, i + 1, j - 1),
+                          matrix[i][j] + dfs(matrix, memo, i + 1, j + 1)});
 
         return memo[i][j];
     }
