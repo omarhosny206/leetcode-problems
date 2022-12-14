1f17d25c96834a58ed9cbd0a9ed9a6a0a22178d7
omarhosny206 <omarhosny102@gmail.com>
2022-12-14 14:59:46 +0200

    Updated problem 198

--- Medium/198. House Robber.cpp
+++ Medium/198. House Robber.cpp
@@ -5,12 +5,11 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        if (nums.size() == 0)
-            return 0;
         if (nums.size() == 1)
             return nums[0];
 
         vector<int> dp(nums.size());
+
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
 
