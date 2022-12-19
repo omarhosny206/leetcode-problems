11f5584b9eacd1ee2ddcf02be780d80cd7601f62
omarhosny206 <omarhosny102@gmail.com>
2022-12-19 22:20:40 +0200

    Updated problem 236

--- Medium/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ Medium/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -17,24 +17,19 @@ public:
     {
         if (!root || root == p || root == q)
             return root;
+
         TreeNode *left = lowestCommonAncestor(root->left, p, q);
         TreeNode *right = lowestCommonAncestor(root->right, p, q);
 
         if (left && right)
-        {
             return root;
-        }
+
         else if (left && !right)
-        {
             return left;
-        }
+
         else if (!left && right)
-        {
             return right;
-        }
-        else
-        {
-            return nullptr;
-        }
+
+        return nullptr;
     }
 };
