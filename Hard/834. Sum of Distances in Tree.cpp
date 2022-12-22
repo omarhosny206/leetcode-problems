406213a44ccd863f9fa47e27316170bc3fb9484d
omarhosny206 <omarhosny102@gmail.com>
2022-12-22 18:15:07 +0200

    Added problem 834

---
+++ Hard/834. Sum of Distances in Tree.cpp
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/sum-of-distances-in-tree/
+
+class Solution
+{
+    vector<int> result;
+    int depthCounter = 0;
+
+public:
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>> &edges)
+    {
+        result = vector<int>(n);
+        vector<vector<int>> graph(n);
+
+        for (vector<int> &edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n);
+        dfs(graph, visited, 0, 0);
+
+        result[0] = depthCounter;
+
+        fill(visited.begin(), visited.end(), false);
+        vector<int> numNodes(n);
+        countNumNodes(graph, visited, numNodes, 0);
+
+        fill(visited.begin(), visited.end(), false);
+        countDistances(graph, visited, numNodes, 0, result[0] + numNodes[0]);
+
+        return result;
+    }
+
+    void countDistances(vector<vector<int>> &graph, vector<bool> &visited, vector<int> &numNodes, int source, int parentDistance)
+    {
+        visited[source] = true;
+        result[source] = parentDistance - numNodes[source] + (graph.size() - numNodes[source]);
+
+        for (int &destination : graph[source])
+            if (!visited[destination])
+                countDistances(graph, visited, numNodes, destination, result[source]);
+    }
+
+    int countNumNodes(vector<vector<int>> &graph, vector<bool> &visited, vector<int> &numNodes, int source)
+    {
+        int result = 0;
+
+        if (visited[source])
+            return 0;
+
+        visited[source] = true;
+
+        for (int &destination : graph[source])
+            result += countNumNodes(graph, visited, numNodes, destination);
+
+        numNodes[source] = result + 1;
+        return numNodes[source];
+    }
+
+    void dfs(vector<vector<int>> &graph, vector<bool> &visited, int source, int depth)
+    {
+        visited[source] = true;
+        depthCounter += depth;
+
+        for (int &destination : graph[source])
+            if (!visited[destination])
+                dfs(graph, visited, destination, depth + 1);
+    }
+};
