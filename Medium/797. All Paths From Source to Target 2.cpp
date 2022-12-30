b4a084d010822ec2234a3455d074e2fb18136375
omarhosny206 <omarhosny102@gmail.com>
2022-12-30 15:46:14 +0200

    Updated problem 797

---
+++ Medium/797. All Paths From Source to Target 2.cpp
@@ -0,0 +1,31 @@
+
+class Solution
+{
+    vector<vector<int>> paths;
+
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph)
+    {
+        vector<int> currentPath = {0};
+        dfs(graph, 0, currentPath);
+
+        return paths;
+    }
+
+    void dfs(vector<vector<int>> &graph, int source, vector<int> &currentPath)
+    {
+        if (source == graph.size() - 1)
+        {
+            paths.push_back(currentPath);
+            return;
+        }
+
+        for (int &destination : graph[source])
+        {
+            currentPath.push_back(destination);
+            dfs(graph, destination, currentPath);
+        }
+
+        currentPath.pop_back();
+    }
+};
