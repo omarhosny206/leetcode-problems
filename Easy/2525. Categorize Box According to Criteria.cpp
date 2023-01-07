a74adddb7b007bcc0c7aa4f029ce70b4ec9cccbf
omarhosny206 <omarhosny102@gmail.com>
2023-01-07 20:07:45 +0200

    Added problem 2525

---
+++ Easy/2525. Categorize Box According to Criteria.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/categorize-box-according-to-criteria/
+
+class Solution
+{
+public:
+    string categorizeBox(int length, int width, int height, int mass)
+    {
+        bool isBulky = (length >= 1e4) || (width >= 1e4) || (height >= 1e4);
+        long long volume = (long long)length * (long long)width * (long long)height;
+        isBulky = isBulky || (volume >= 1e9);
+
+        bool isHeavy = (mass >= 100);
+
+        if (isBulky && isHeavy)
+            return "Both";
+
+        if (isBulky && !isHeavy)
+            return "Bulky";
+
+        if (!isBulky && isHeavy)
+            return "Heavy";
+
+        return "Neither";
+    }
+};
