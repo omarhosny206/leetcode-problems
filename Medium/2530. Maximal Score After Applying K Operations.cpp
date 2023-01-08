c88757fdd0869bd233f89e6406979594a5b9d775
omarhosny206 <omarhosny102@gmail.com>
2023-01-08 12:25:58 +0200

    Added problem 2530

---
+++ Medium/2530. Maximal Score After Applying K Operations.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximal-score-after-applying-k-operations/
+
+class Solution
+{
+public:
+    long long maxKelements(vector<int> &nums, int k)
+    {
+        long long result = 0;
+        priority_queue<double> values;
+
+        for (int &num : nums)
+            values.push(num);
+
+        while (k--)
+        {
+            double value = values.top();
+            values.pop();
+
+            result += value;
+            value = ceil(value / 3);
+
+            if (value != 0)
+                values.push(value);
+        }
+
+        return result;
+    }
+};
