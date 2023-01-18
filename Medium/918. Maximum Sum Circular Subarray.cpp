75f411f57bb00391cd359fb6c4c5f73f828b177f
omarhosny206 <omarhosny102@gmail.com>
2023-01-18 14:52:56 +0200

    Added problem 918

---
+++ Medium/918. Maximum Sum Circular Subarray.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/maximum-sum-circular-subarray/
+
+class Solution
+{
+public:
+    int maxSubarraySumCircular(vector<int> &nums)
+    {
+        int totalSum = 0;
+
+        int globalMax = nums[0];
+        int currentMax = 0;
+
+        int globalMin = nums[0];
+        int currentMin = 0;
+
+        for (int &num : nums)
+        {
+            currentMax = max(currentMax + num, num);
+            globalMax = max(globalMax, currentMax);
+
+            currentMin = min(currentMin + num, num);
+            globalMin = min(globalMin, currentMin);
+
+            totalSum += num;
+        }
+
+        bool areAllNegatives = globalMax <= 0;
+        if (areAllNegatives)
+            return globalMax;
+
+        return max(globalMax, totalSum - globalMin);
+    }
+};
