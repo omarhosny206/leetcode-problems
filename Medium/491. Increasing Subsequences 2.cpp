4a31673959a5e1622973aca62d7a4687f1d7aafe
omarhosny206 <omarhosny102@gmail.com>
2023-01-20 16:11:10 +0200

    Added problem 491 2

---
+++ Medium/491. Increasing Subsequences 2.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/increasing-uniqueSubsequences/
+
+class Solution
+{
+    vector<vector<int>> result;
+    set<vector<int>> uniqueSubsequences;
+
+public:
+    vector<vector<int>> findSubsequences(vector<int> &nums)
+    {
+        vector<int> current;
+
+        dfs(nums, current, 0);
+
+        for (const vector<int> &uniqueSubsequence : uniqueSubsequences)
+            result.push_back(uniqueSubsequence);
+
+        return result;
+    }
+
+    void dfs(vector<int> &nums, vector<int> &current, int start)
+    {
+        if (current.size() >= 2 && uniqueSubsequences.find(current) == uniqueSubsequences.end())
+            uniqueSubsequences.insert(current);
+
+        if (start >= nums.size())
+            return;
+
+        for (int i = start; i < nums.size(); ++i)
+        {
+            if (current.empty() || (!current.empty() && nums[i] >= current.back()))
+            {
+                current.push_back(nums[i]);
+                dfs(nums, current, i + 1);
+                current.pop_back();
+            }
+        }
+    }
+};
