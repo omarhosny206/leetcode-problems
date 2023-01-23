2a00129074c6bd7fdf0b098a2480c3a9b589e6c7
omarhosny206 <omarhosny102@gmail.com>
2023-01-23 15:11:31 +0200

    Updated problem 977

--- Easy/977. Squares of a Sorted Array.cpp
+++ Easy/977. Squares of a Sorted Array.cpp
@@ -3,49 +3,18 @@
 class Solution
 {
 public:
-    vector<int> sortedSquares(vector<int> &A)
+    vector<int> sortedSquares(vector<int> &nums)
     {
-        for (int &num : A)
-            num = num * num;
-        quickSort(A, 0, A.size() - 1);
-        return A;
+        sort(nums.begin(), nums.end(), compare);
+
+        for (int i = 0; i < nums.size(); ++i)
+            nums[i] *= nums[i];
+
+        return nums;
     }
 
-    void quickSort(vector<int> &nums, int left, int right)
+    static bool compare(int &first, int &second)
     {
-        if (left < right)
-        {
-            int pivotIndex = partition(nums, left, right);
-            quickSort(nums, left, pivotIndex - 1);
-            quickSort(nums, pivotIndex, right);
-        }
-    }
-
-    int partition(vector<int> &nums, int left, int right)
-    {
-        int pivotIndex = left + (right - left) / 2;
-        int pivotValue = nums[pivotIndex];
-        int i = left, j = right;
-        int temp;
-        while (i <= j)
-        {
-            while (nums[i] < pivotValue)
-            {
-                i++;
-            }
-            while (nums[j] > pivotValue)
-            {
-                j--;
-            }
-            if (i <= j)
-            {
-                temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j--;
-            }
-        }
-        return i;
+        return (first * first) < (second * second);
     }
 };
