ccfe54061679ba332452ed5c4670fa3f6b8f9f79
omarhosny206 <omarhosny102@gmail.com>
2023-01-30 14:43:49 +0200

    Updated problem 1137

--- Easy/1137. N-th Tribonacci Number.cpp
+++ Easy/1137. N-th Tribonacci Number.cpp
@@ -2,21 +2,25 @@
 
 class Solution
 {
+private:
+    unordered_map<int, int> dp;
+
 public:
-    unordered_map<int, int> table;
     int tribonacci(int n)
     {
-        if (table.find(n) != table.end())
-            return table[n];
-
         if (n == 0)
             return 0;
+
         if (n == 1)
             return 1;
+
         if (n == 2)
             return 1;
-        int result = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
-        table[n] = result;
-        return result;
+
+        if (dp.find(n) != dp.end())
+            return dp[n];
+
+        dp[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        return dp[n];
     }
 };
