dde607e6e8e26bb4e523b73a7ed9762363e74f87
omarhosny206 <omarhosny102@gmail.com>
2023-01-30 14:43:42 +0200

    Updated problem 1137 2

--- Easy/1137. N-th Tribonacci Number 2.cpp
+++ Easy/1137. N-th Tribonacci Number 2.cpp
@@ -7,18 +7,23 @@ public:
     {
         if (n == 0)
             return 0;
+
         if (n == 1 || n == 2)
             return 1;
 
-        int x = 0, xx = 1, xxx = 1;
         int result = 0;
+        int first = 0;
+        int second = 1;
+        int third = 1;
+
         for (int i = 3; i <= n; ++i)
         {
-            result = x + xx + xxx;
-            x = xx;
-            xx = xxx;
-            xxx = result;
+            result = first + second + third;
+            first = second;
+            second = third;
+            third = result;
         }
+
         return result;
     }
 };
