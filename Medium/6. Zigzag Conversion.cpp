a4da72fb4972748a12c4a0860f929754af85ec30
omarhosny206 <omarhosny102@gmail.com>
2023-02-03 17:07:17 +0200

    Added problem 6

---
+++ Medium/6. Zigzag Conversion.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/zigzag-conversion/
+
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+
+        string result = "";
+        vector<string> words(numRows);
+        
+        int row = 0;
+        bool moveToDown = true;
+
+        for (char &c : s)
+        {
+            words[row] += c;
+
+            if (row == 0)
+                moveToDown = true;
+
+            if (row == numRows - 1)
+                moveToDown = false;
+
+            row = (moveToDown) ? row + 1 : row - 1;
+        }
+
+        for (string &word : words)
+            result += word;
+
+        return result;
+    }
+};
