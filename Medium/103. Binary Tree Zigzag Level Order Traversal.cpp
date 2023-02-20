5d1c7e148261e8eb96d287f9989783384ec5c58d
omarhosny206 <omarhosny102@gmail.com>
2023-02-20 14:15:27 +0200

    Added problem 103

---
+++ Medium/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,61 @@
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        return bfs(root);
+    }
+
+    vector<vector<int>> bfs(TreeNode *root)
+    {
+        if (root == nullptr)
+            return {};
+
+        vector<vector<int>> result;
+        queue<TreeNode *> nodes;
+        bool shouldBeReversed = false;
+
+        nodes.push(root);
+
+        while (!nodes.empty())
+        {
+            int size = nodes.size();
+            vector<int> values;
+
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode *current = nodes.front();
+                nodes.pop();
+
+                if (current->left)
+                    nodes.push(current->left);
+
+                if (current->right)
+                    nodes.push(current->right);
+
+                values.push_back(current->val);
+            }
+
+            if (shouldBeReversed)
+                reverse(values.begin(), values.end());
+
+            result.push_back(values);
+            shouldBeReversed = !shouldBeReversed;
+        }
+
+        return result;
+    }
+};
