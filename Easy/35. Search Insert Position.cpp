e68abcbd8b480c08e2c9f14be38afc3d7bd24f58
omarhosny206 <omarhosny102@gmail.com>
2023-02-20 13:09:59 +0200

    Updated problem 35

--- Easy/35. Search Insert Position.cpp
+++ Easy/35. Search Insert Position.cpp
@@ -5,29 +5,22 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int target)
     {
-        int position = binarySearch(nums, target, 0, nums.size() - 1);
-        if (position == -1)
-        {
-            int i = 0;
-            while (i < nums.size() && nums[i] < target)
-                i++;
-            position = i;
-        }
-
-        return position;
+        return binarySearch(nums, target, 0, nums.size() - 1);
     }
 
     int binarySearch(vector<int> &nums, int target, int left, int right)
     {
         if (left > right)
-            return -1;
-        int middle = left + (right - middle) / 2;
+            return left;
+
+        int middle = left + (right - left) / 2;
 
         if (nums[middle] == target)
             return middle;
-        else if (nums[middle] > target)
-            return binarySearch(nums, target, left, middle - 1);
-        else
+
+        if (nums[middle] < target)
             return binarySearch(nums, target, middle + 1, right);
+
+        return binarySearch(nums, target, left, middle - 1);
     }
 };
