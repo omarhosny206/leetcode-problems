20dbbae04ecb5abf196abf81e5ee50d9f5c303ba
omarhosny206 <omarhosny102@gmail.com>
2023-02-23 12:09:55 +0200

    Added problem 502

---
+++ Hard/502. IPO.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/ipo/
+
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+    {
+        int result;
+        priority_queue<pair<int, int>> maxProfits;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minCapitals;
+
+        for (int i = 0; i < profits.size(); ++i)
+            minCapitals.push({capital[i], profits[i]});
+
+        while (k--)
+        {
+            while (!minCapitals.empty() && minCapitals.top().first <= w)
+            {
+                pair<int, int> value = minCapitals.top();
+                minCapitals.pop();
+
+                maxProfits.push({value.second, value.first});
+            }
+
+            if (!maxProfits.empty())
+            {
+                pair<int, int> value = maxProfits.top();
+                maxProfits.pop();
+
+                w += value.first;
+            }
+        }
+
+        result = w;
+        return result;
+    }
+};
