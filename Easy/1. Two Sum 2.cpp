503e107f09cf0bbfa47d03031cd0d3ffd83b1ab5
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-06-04 21:43:15 +0300

    Update 1. Two Sum 2.cpp

--- Easy/1. Two Sum 2.cpp
+++ Easy/1. Two Sum 2.cpp
@@ -5,15 +5,14 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> result;
-        unordered_map<int, int> seen;
+        unordered_map<int, int> index;
 
         for (int i = 0; i < nums.size(); ++i)
         {
-            if (seen.find(nums[i]) != seen.end())
-                return {seen[nums[i]], i};
+            if (index.find(target - nums[i]) != index.end())
+                return {index[target - nums[i]], i};
 
-            seen[target - nums[i]] = i;
+            index[nums[i]] = i;
         }
 
         return {};
